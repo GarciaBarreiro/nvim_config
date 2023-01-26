6a23659847a6f53206d0c5ddc551c589354130b0
Tomas G B <tomas.garcia.prof@gmail.com>
2023-01-26 18:39:52 +0100

    Fixed some things about Telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,4 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})	-- finds all files
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})		-- finds git files (Ctrl p)
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end) 								-- searchs strings using grep (doesn't work??)
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})		    -- finds git files (Ctrl p)
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})    -- searchs strings using grep (requires ripgrep)
--- lua/frd/init.lua
+++ lua/frd/init.lua
@@ -1,2 +1,4 @@
 require("frd.remap")
 require("frd.set")
+
+vim.api.nvim_set_hl(0, "TelescopeNormal", {bg="#000000"})
